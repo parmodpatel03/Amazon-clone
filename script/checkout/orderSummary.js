cb4df0e11a969a84ea1f95ad2a73138eddd6866a
parmodpatel03 <parmodpatel2603@gmail.com>
2025-02-06 02:38:08 +0530

    make the checkout page interactive completely

--- script/checkout/orderSummary.js
+++ script/checkout/orderSummary.js
@@ -1,30 +1,22 @@
 import {cart,removeFromCart,calculateCartQuantity,updateQuantity,updateDeliveryOption} from '../../data/cart.js'
-import {products} from '../../data/products.js'
+import {products,getProduct} from '../../data/products.js'
 import {formatCurrency} from '../utils/money.js'
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js'
-import {deliveryOptions} from '../../data/deliveryOptions.js'
+import {deliveryOptions,getDeliveryOption} from '../../data/deliveryOptions.js'
+import {renderPaymentSummary} from '../checkout/paymentSummary.js'
 
 export  function renderOrderSummary() {
   let cartSummaryHTML = ''
+  
 
   cart.forEach((cartItem) => {
 
     const productId = cartItem.productId
-    let matchingProduct
-    products.forEach((product) => {
-      if(product.id === productId) {
-        matchingProduct = product
-      }
-    })
+    const matchingProduct = getProduct(productId)
 
 
     const deliveryOptionId = cartItem.deliveryOptionId
-    let deliveryOption;
-    deliveryOptions.forEach((option) => {
-      if(option.id === deliveryOptionId) {
-        deliveryOption = option
-      }
-    })
+    const deliveryOption = getDeliveryOption(deliveryOptionId)
     const today = dayjs()
     const deliveryDate = today.add(deliveryOption.deliveryDays,'days')
     const dateString = deliveryDate.format('dddd, MMMM D')
@@ -122,6 +114,7 @@ export  function renderOrderSummary() {
         const container = document.querySelector(`.js-cart-item-container-${productId}`)
         container.remove()
         updateCartQuantity()
+        renderPaymentSummary()
     })  
   })
 
@@ -169,7 +162,7 @@ export  function renderOrderSummary() {
         const quantityLabel = document.querySelector(`.js-quantity-label-${productId}`)
         quantityLabel.innerHTML = newQuantity
         updateCartQuantity()
-
+        renderPaymentSummary()
       })
     })
 
@@ -180,6 +173,7 @@ export  function renderOrderSummary() {
         const {productId,deliveryOptionId} = element.dataset
         updateDeliveryOption(productId,deliveryOptionId)
         renderOrderSummary()
+        renderPaymentSummary()
       })
     })
 
