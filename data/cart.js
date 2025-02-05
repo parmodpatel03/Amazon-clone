3566f4bb886e2b20df1c29074a36441d397fee08
parmodpatel03 <parmodpatel2603@gmail.com>
2025-02-06 01:42:44 +0530

    make delivery options interactive

--- data/cart.js
+++ data/cart.js
@@ -77,3 +77,16 @@ export function updateQuantity(productId,newQuantity) {
     saveToStorage()
   })
 }
+
+export function updateDeliveryOption(productId ,deliveryOptionId) {
+  let matchingItem;
+
+  cart.forEach((item) => {
+    if(productId === item.productId){
+      matchingItem = item
+    }
+  })
+  matchingItem.deliveryOptionId = deliveryOptionId
+
+  saveToStorage()
+}
